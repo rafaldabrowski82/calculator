dbcfa21d8d3965f20a4999deef346387bcaad579
Rafal Dabrowski <rafaldabrowski82@gmail.com>
2020-03-09 14:56:16 +0100

    I finished the view of application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    render() {
+
+        return (
+            <div className="calculator">
+                <div className="display">
+                    <p>0</p>
+                </div>
+                <div className="digit">
+                    <button className="color-function">AC</button>
+                    <button className="color-function">&#177;</button>
+                    <button className="color-function">%</button>
+                    <button className="color-operation">&#247;</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button className="color-operation">&#215;</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button className="color-operation">-</button>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button className="color-operation">+</button>
+                    <button className="zero">0</button>
+                    <button>.</button>
+                    <button className="color-operation">=</button>
+                </div>
+            </div>
+        );
+
+    }
 }
 
 export default App;
